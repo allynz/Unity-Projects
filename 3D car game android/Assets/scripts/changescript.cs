3422175c1b226b5ed11a887c80080de273dbf562
LeastCommonDivisor <LeastCommonDivisor@users.noreply.github.com>
2019-02-10 15:13:00 +0530

    Made first game(car simulation) for android
    Used Unity StandardAssets for car, and terrain for the plane, added some trees, buttons and the project is ready!
    Can do much more, like add audio etc.
    Also includes apk file for android as platform was android

---
+++ 3D car game android/Assets/scripts/changescript.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class changescript : MonoBehaviour
+{
+    
+    public void changescene(string s)
+    {
+        SceneManager.LoadScene(s);
+    }
+
+    public void quitgame()
+    {
+        Application.Quit();
+    }
+
+}
